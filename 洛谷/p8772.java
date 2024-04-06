33441fd5cba5bbe46f52a21c2d21b991ca39fe42
fireworks <3245375160@qq.com>
2024-04-06 13:43:19 +0800

    前缀和（差分数组）蓝桥杯真题

--- 洛谷/p8772.java
+++ 洛谷/p8772.java
@@ -1,5 +1,24 @@
 package 洛谷;
 
-public class p8772 {
+import java.util.Scanner;
 
+public class p8772 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int n=scan.nextInt();
+	int a[]=new int[n];
+	long ans=0;
+	long sum[]=new long[n+1];
+	for (int i = 0; i <n; i++) {
+		a[i]=scan.nextInt();//读入原数组
+	}
+	for (int i = 1; i <=n; i++) {//初始化前缀和数组
+		sum[i]=sum[i-1]+a[i-1];
+	}
+	for (int i = 0; i <n; i++) {
+		ans+=(a[i]*(sum[n]-sum[i+1]));
+	}
+	System.out.println(ans);
+	scan.close();
+}
 }
--- 经典算法/一维数组前缀和.java
+++ 经典算法/一维数组前缀和.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
  * 这时可以使用前缀和思想优化算法
  * 前缀和仅需一次时间复杂度为On的预处理，后续查询时间复杂度为O1
  * 本质是把每次运算结果记录在前缀和数组中，以空间换时间
- * 
+ * 前缀和（差分数组）数很大，开long！开long！！开long！！！
  * */
 public class 一维数组前缀和 {
 public static void main(String[] args) {
 	Scanner scan = new Scanner(System.in);
 	int num[]=new int[] {1,2,3,4,5,6,7,8,9,10};//目标数组
-	int sum[]=new int[11];
+	int sum[]=new int[11];//前缀和（差分数组）开long，切记，切记！这里不开了嫌麻烦
 	for (int i = 0; i < 10; i++) {
 		sum[i+1]=num[i]+sum[i];
 	}
 	System.err.println(sum[10]);
-	System.err.println(sum[10]-sum[8]);
+	System.err.println(sum[10]-sum[8]);//以On的时间复杂度计算了10+9
 	int ans=0;
 	for (int i = 0; i <num.length; i++) {
 		ans+=num[i];
