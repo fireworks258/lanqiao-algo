252c8b16f0cbf4242782495af0b57465ee95692f
fireworks <3245375160@qq.com>
2024-04-06 21:23:17 +0800

    2023.4.6,要死了

---
+++ 洛谷/p8649k倍区间_前缀和.java
@@ -0,0 +1,49 @@
+package 洛谷;
+
+import java.util.Scanner;
+public class p8649k倍区间_前缀和 {//没搞明白，有待研究
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int n=scan.nextInt();
+//		int a[]=new int[n];
+//		int k=scan.nextInt();
+//		int ans=0;
+//		int value[]=new int[100005];
+//		int sum[]=new int[n+1];//前缀和数组,大小比原数组大一位,用lang存储
+//		for (int i = 0; i <n; i++) {//读入数据
+//			a[i]=scan.nextInt();
+//		}
+//		for (int i = 1; i <=n; i++) {//计算前缀和
+//			sum[i]=sum[i-1]+a[i-1];
+//		}
+///*-----------暴力解法，超时---------------------------------*/
+////		for (int i = 1; i <=n; i++) {//i为区间长度
+////			for (int j = i; j <=n; j++) {//j为右端点
+////				if((sum[j]-sum[j-i])%k==0) {
+////					ans++;
+////				}
+////			}
+////		}
+///*----------使用同余定理 a%n=x且b%n=x可推得(a-b)%n=0----*/
+//		for (int i = 0; i <=n; i++) {
+//			 ans+=value[(sum[i]%k)]++;
+//		}
+//		System.out.println(ans);
+//		scan.close();
+//	}
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	int n=scan.nextInt();
+	int k=scan.nextInt();
+	int f[]=new int[k];
+	f[0]=1;
+	long sum=0,ans=0;
+	for (int i = 0; i <n; i++) {
+		sum+=scan.nextLong();
+		ans+=f[(int)(sum%k)];
+		f[(int)(sum%k)]++;
+	}
+	System.out.println(ans);
+	scan.close();
+	}
+}
--- 洛谷/p8772.java
+++ 洛谷/p8772.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	int n=scan.nextInt();
 	int a[]=new int[n];
 	long ans=0;
-	long sum[]=new long[n+1];
+	long sum[]=new long[n+1];//前缀和（差分数组）开long，切记，切记！
 	for (int i = 0; i <n; i++) {
 		a[i]=scan.nextInt();//读入原数组
 	}
